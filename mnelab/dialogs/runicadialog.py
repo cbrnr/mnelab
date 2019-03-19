bb66fcb93463953ec6eaedce9bb7ad9deea20376
Clemens Brunner <clemens.brunner@gmail.com>
2019-03-19 10:23:27 +0100

    Include options for Infomax and Picard in dialog window

--- mnelab/dialogs/runicadialog.py
+++ mnelab/dialogs/runicadialog.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QGridLayout, QLabel,
                              QSpinBox, QComboBox, QDialogButtonBox, QCheckBox)
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSlot
 
 
 class RunICADialog(QDialog):
@@ -11,20 +11,41 @@ class RunICADialog(QDialog):
         grid = QGridLayout()
         grid.addWidget(QLabel("Method:"), 0, 0)
         self.method = QComboBox()
-        self.methods = {"Extended Infomax": "extended-infomax",
-                   "Infomax": "infomax", "FastICA": "fastica"}
+        self.methods = {"Infomax": "infomax", "FastICA": "fastica"}
         if have_picard:
             self.methods["Picard"] = "picard"
         self.method.addItems(self.methods.keys())
+        if have_picard:
+            self.method.setCurrentText("Picard")
+        else:
+            self.method.setCurrentText("Infomax")
+        min_len = max(len(key) for key in self.methods.keys())
+        self.method.setMinimumContentsLength(min_len)
         grid.addWidget(self.method, 0, 1)
-        grid.addWidget(QLabel("Number of components:"), 1, 0)
+
+        self.extended_label = QLabel("Extended:")
+        grid.addWidget(self.extended_label, 1, 0)
+        self.extended = QCheckBox()
+        self.extended.setChecked(True)
+        grid.addWidget(self.extended, 1, 1)
+
+        self.ortho_label = QLabel("Orthogonal:")
+        grid.addWidget(self.ortho_label, 2, 0)
+        self.ortho = QCheckBox()
+        self.ortho.setChecked(False)
+        grid.addWidget(self.ortho, 2, 1)
+
+        self.toggle_options()
+        self.method.currentIndexChanged.connect(self.toggle_options)
+
+        grid.addWidget(QLabel("Number of components:"), 3, 0)
         self.n_components = QSpinBox()
         self.n_components.setMinimum(0)
         self.n_components.setMaximum(nchan)
         self.n_components.setValue(nchan)
         self.n_components.setAlignment(Qt.AlignRight)
-        grid.addWidget(self.n_components, 1, 1)
-        grid.addWidget(QLabel("Exclude bad segments:"), 2, 0)
+        grid.addWidget(self.n_components, 3, 1)
+        grid.addWidget(QLabel("Exclude bad segments:"), 4, 0)
         self.exclude_bad_segments = QCheckBox()
         self.exclude_bad_segments.setChecked(True)
         grid.addWidget(self.exclude_bad_segments)
@@ -34,3 +55,24 @@ class RunICADialog(QDialog):
         vbox.addWidget(buttonbox)
         buttonbox.accepted.connect(self.accept)
         buttonbox.rejected.connect(self.reject)
+        vbox.setSizeConstraint(QVBoxLayout.SetFixedSize)
+
+    @pyqtSlot()
+    def toggle_options(self):
+        """Toggle extended options.
+        """
+        if self.method.currentText() == "Picard":
+            self.extended_label.show()
+            self.extended.show()
+            self.ortho_label.show()
+            self.ortho.show()
+        elif self.method.currentText() == "Infomax":
+            self.extended_label.show()
+            self.extended.show()
+            self.ortho_label.hide()
+            self.ortho.hide()
+        else:
+            self.extended_label.hide()
+            self.extended.hide()
+            self.ortho_label.hide()
+            self.ortho.hide()
