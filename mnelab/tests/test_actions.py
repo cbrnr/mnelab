f6806f2fb9078c0bad8f276284d3c8dd8e36d11e
Clemens Brunner <clemens.brunner@gmail.com>
2019-04-19 09:50:36 +0200

    Disable qtbot

--- mnelab/tests/test_actions.py
+++ mnelab/tests/test_actions.py
@@ -1,12 +1,12 @@
 from mnelab import MainWindow, Model
 
 
-def test_initial_actions(qtbot):
+def test_initial_actions():
     """Test if initial actions are correctly enabled/disabled."""
     model = Model()
     view = MainWindow(model)
     model.view = view
-    qtbot.addWidget(view)
+    # qtbot.addWidget(view)
 
     for name, action in view.actions.items():
         if name in view.always_enabled:
