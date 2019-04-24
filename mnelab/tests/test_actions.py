55f698242a0a693b43ff3376cdf5be15977fe995
Clemens Brunner <clemens.brunner@gmail.com>
2019-04-24 11:03:19 +0200

    Minimal example (passes locally)

--- mnelab/tests/test_actions.py
+++ mnelab/tests/test_actions.py
@@ -1,17 +1,21 @@
-from mnelab import MainWindow, Model
-from PyQt5.QtWidgets import QWidget, QLabel
+# from mnelab import MainWindow, Model
+from PyQt5.QtWidgets import QWidget
 
 
-def test_initial_actions(qtbot):
-    """Test if initial actions are correctly enabled/disabled."""
-    assert True
-    model = Model()
-    view = MainWindow(model)
-    model.view = view
-    # qtbot.addWidget(view)
+# def test_initial_actions(qtbot):
+#     """Test if initial actions are correctly enabled/disabled."""
+#     assert True
+#     model = Model()
+#     view = MainWindow(model)
+#     model.view = view
+#     # qtbot.addWidget(view)
+#
+#     for name, action in view.actions.items():
+#         if name in view.always_enabled:
+#             assert action.isEnabled()
+#         else:
+#             assert not action.isEnabled()
 
-    for name, action in view.actions.items():
-        if name in view.always_enabled:
-            assert action.isEnabled()
-        else:
-            assert not action.isEnabled()
+
+def test_pyqt(qtbot):
+    widget = QWidget()
