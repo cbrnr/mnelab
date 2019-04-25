1661b203958fee5e95be8804963e53c83f05a289
Clemens Brunner <clemens.brunner@gmail.com>
2019-04-25 08:48:30 +0200

    Run real test

--- mnelab/tests/test_actions.py
+++ mnelab/tests/test_actions.py
@@ -1,21 +1,16 @@
-# from mnelab import MainWindow, Model
-from PyQt5.QtWidgets import QWidget
+from mnelab import MainWindow, Model
 
 
-# def test_initial_actions(qtbot):
-#     """Test if initial actions are correctly enabled/disabled."""
-#     assert True
-#     model = Model()
-#     view = MainWindow(model)
-#     model.view = view
-#     # qtbot.addWidget(view)
-#
-#     for name, action in view.actions.items():
-#         if name in view.always_enabled:
-#             assert action.isEnabled()
-#         else:
-#             assert not action.isEnabled()
+def test_initial_actions(qtbot):
+    """Test if initial actions are correctly enabled/disabled."""
+    assert True
+    model = Model()
+    view = MainWindow(model)
+    model.view = view
+    qtbot.addWidget(view)
 
-
-def test_pyqt(qtbot):
-    widget = QWidget()
+    for name, action in view.actions.items():
+        if name in view.always_enabled:
+            assert action.isEnabled()
+        else:
+            assert not action.isEnabled()
