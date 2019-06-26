dff3a9a17a5481ce4efed7528597fd9050b2c517
Clemens Brunner <clemens.brunner@gmail.com>
2019-06-26 12:59:39 +0200

    Add/remove events (#25)
    * Use method in model to set events (automatically updates the view)
    
    * Removing selected rows works
    
    * Adding events also works

--- mnelab/dialogs/eventsdialog.py
+++ mnelab/dialogs/eventsdialog.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QDialogButtonBox,
-                             QTableWidget, QTableWidgetItem, QAbstractItemView)
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QHBoxLayout,
+                             QDialogButtonBox, QTableWidget, QTableWidgetItem,
+                             QAbstractItemView, QPushButton)
+from PyQt5.QtCore import Qt, pyqtSlot
 
 
 class IntTableWidgetItem(QTableWidgetItem):
@@ -35,15 +36,53 @@ class EventsDialog(QDialog):
         self.table.horizontalHeader().setStretchLastSection(True)
         self.table.verticalHeader().setVisible(False)
         self.table.setShowGrid(False)
-        self.table.setSelectionMode(QAbstractItemView.NoSelection)
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.table.setSortingEnabled(True)
         self.table.sortByColumn(0, Qt.AscendingOrder)
 
         vbox = QVBoxLayout(self)
         vbox.addWidget(self.table)
+        hbox = QHBoxLayout()
+        self.add_button = QPushButton("+")
+        self.remove_button = QPushButton("-")
         buttonbox = QDialogButtonBox(QDialogButtonBox.Ok |
                                      QDialogButtonBox.Cancel)
-        vbox.addWidget(buttonbox)
+        hbox.addWidget(self.add_button)
+        hbox.addWidget(self.remove_button)
+        hbox.addStretch()
+        hbox.addWidget(buttonbox)
+        vbox.addLayout(hbox)
         buttonbox.accepted.connect(self.accept)
         buttonbox.rejected.connect(self.reject)
+        self.table.itemSelectionChanged.connect(self.toggle_buttons)
+        self.remove_button.clicked.connect(self.remove_event)
+        self.add_button.clicked.connect(self.add_event)
+        self.toggle_buttons()
         self.resize(300, 500)
+
+    @pyqtSlot()
+    def toggle_buttons(self):
+        """Toggle + and - buttons."""
+        if len(self.table.selectedItems()) == 2:  # one row (2 items) selected
+            self.add_button.setEnabled(True)
+            self.remove_button.setEnabled(True)
+        elif len(self.table.selectedItems()) > 2:  # more than one row selected
+            self.add_button.setEnabled(False)
+            self.remove_button.setEnabled(True)
+        else:  # no rows selected
+            self.add_button.setEnabled(False)
+            self.remove_button.setEnabled(False)
+
+    def add_event(self):
+        current_row = self.table.selectedIndexes()[0].row()
+        pos = int(self.table.item(current_row, 0).data(Qt.DisplayRole))
+        self.table.setSortingEnabled(False)
+        self.table.insertRow(current_row)
+        self.table.setItem(current_row, 0, IntTableWidgetItem(pos))
+        self.table.setItem(current_row, 1, IntTableWidgetItem(0))
+        self.table.setSortingEnabled(True)
+
+    def remove_event(self):
+        rows = {index.row() for index in self.table.selectedIndexes()}
+        for row in sorted(rows, reverse=True):
+            self.table.removeRow(row)
