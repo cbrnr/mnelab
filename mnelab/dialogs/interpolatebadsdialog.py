421a58c4f918398af24da15d822dfc2585c85ad3
victor ferat <victor.ferat@live.fr>
2019-07-15 18:31:07 +0200

    ADD interpolate bad channels

---
+++ mnelab/dialogs/interpolatebadsdialog.py
@@ -0,0 +1,54 @@
+from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QGridLayout, QLabel,
+                             QLineEdit, QDialogButtonBox, QComboBox,
+                             QCheckBox, QDoubleSpinBox)
+
+
+class InterpolateBadsDialog(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setWindowTitle("Filter data")
+        vbox = QVBoxLayout(self)
+        grid = QGridLayout()
+        grid.addWidget(QLabel("Reset bads:"), 0, 0)
+        self.reset_bads_checkbox = QCheckBox()
+        self.reset_bads_checkbox.setChecked(True)
+        grid.addWidget(self.reset_bads_checkbox, 0, 1)
+        grid.addWidget(QLabel("Mode:"), 1, 0)
+        self.mode_select = QComboBox()
+        self.modes = {"Accurate": "accurate", "Fast": "fast"}
+        self.mode_select.addItems(self.modes.keys())
+        self.mode_select.setCurrentText("Accurate")
+        grid.addWidget(self.mode_select, 1, 1)
+        grid.addWidget(QLabel("Origin (x,y,z):"), 2, 0)
+        self.x = QDoubleSpinBox()
+        self.x.setValue(0.)
+        grid.addWidget(self.x, 2, 1)
+        self.y = QDoubleSpinBox()
+        self.y.setValue(0.)
+        grid.addWidget(self.y, 2, 2)
+        self.z = QDoubleSpinBox()
+        self.z.setValue(0.04)
+        grid.addWidget(self.z, 2, 3)
+
+        vbox.addLayout(grid)
+        buttonbox = QDialogButtonBox(QDialogButtonBox.Ok |
+                                     QDialogButtonBox.Cancel)
+        vbox.addWidget(buttonbox)
+        buttonbox.accepted.connect(self.accept)
+        buttonbox.rejected.connect(self.reject)
+        vbox.setSizeConstraint(QVBoxLayout.SetFixedSize)
+
+    @property
+    def origin(self):
+        x = float(self.x.text())
+        y = float(self.y.text())
+        z = float(self.z.text())
+        return [x,y,z]
+
+    @property
+    def mode(self):
+        return str(self.mode_select.currentText())
+
+    @property
+    def reset_bads(self):
+        return bool(self.reset_bads_checkbox.isChecked())
--- mnelab/mainwindow.py
+++ mnelab/mainwindow.py
@@ -12,6 +12,7 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QFileDialog, QSplitter,
                              QStatusBar, QToolBar)
 from mne.io.pick import channel_type
 
+from .dialogs.interpolatebadsdialog import InterpolateBadsDialog
 from .dialogs.annotationsdialog import AnnotationsDialog
 from .dialogs.filterdialog import FilterDialog
 from .dialogs.findeventsdialog import FindEventsDialog
@@ -202,6 +203,10 @@ class MainWindow(QMainWindow):
         self.actions["apply_ica"] = tools_menu.addAction("Apply &ICA",
                                                          self.apply_ica)
         tools_menu.addSeparator()
+        self.actions["interpolate_bads"] = tools_menu.addAction(
+                                                "&Interpolate bad channels...",
+                                                self.interpolate_bads)
+        tools_menu.addSeparator()
         self.actions["epoch_data"] = tools_menu.addAction(
             "Create Epochs...", self.epoch_data)
 
@@ -294,6 +299,7 @@ class MainWindow(QMainWindow):
             self.actions["plot_ica_components"].setEnabled(enabled and ica and
                                                            montage)
             self.actions["plot_ica_sources"].setEnabled(enabled and ica)
+            self.actions["interpolate_bads"].setEnabled(enabled and montage)
             self.actions["events"].setEnabled(enabled and events)
             self.actions["events_from_annotations"].setEnabled(enabled and
                                                                annot)
@@ -554,6 +560,14 @@ class MainWindow(QMainWindow):
         """Apply current fitted ICA."""
         self.auto_duplicate()
         self.model.apply_ica()
+
+    def interpolate_bads(self):
+        """Interpoalte bad channels"""
+        dialog = InterpolateBadsDialog(self)
+        if dialog.exec_():
+            self.auto_duplicate()
+            self.model.interpolate_bads(dialog.reset_bads, dialog.mode,
+                                        dialog.origin)
 
     def filter_data(self):
         """Filter data."""
