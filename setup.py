993faadd9d40b4eda9d5ad1f2e0120d35a1259e7
Clemens Brunner <clemens.brunner@gmail.com>
2020-12-22 22:21:58 +0100

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -63,7 +63,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     keywords='EEG MEG MNE GUI electrophysiology',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
