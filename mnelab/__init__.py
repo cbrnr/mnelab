9d627e6d55a0d04dd4653c0f887fa8d538d2433f
Clemens Brunner <clemens.brunner@gmail.com>
2021-07-30 17:18:03 +0200

    Moving __version__ down

--- mnelab/__init__.py
+++ mnelab/__init__.py
@@ -2,10 +2,9 @@
 #
 # License: BSD (3-clause)
 
-__version__ = "0.7.0.dev0"
-
 from .mainwindow import MainWindow
 from .model import Model
 
 
+__version__ = "0.7.0.dev0"
 __all__ = [MainWindow, Model]
