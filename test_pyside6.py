cfbbc59246e1e2f4636aad394a941b8dab00ad55
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-16 19:16:46 +0100

    Quit app after 1000ms

--- test_pyside6.py
+++ test_pyside6.py
@@ -1,6 +1,8 @@
+from PySide2.QtCore import QTimer, SLOT
 from PySide2.QtWidgets import QApplication, QLabel
 
 app = QApplication()
 label = QLabel("1")
 label.show()
+QTimer.singleShot(1000, app, SLOT("quit()"))
 app.exec_()
