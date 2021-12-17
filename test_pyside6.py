d646cc4e31ead3ed4eb4f864203f6a1ba79de46d
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-17 08:51:10 +0100

    New-style connection

--- test_pyside6.py
+++ test_pyside6.py
@@ -1,8 +1,8 @@
-from PySide6.QtCore import QTimer, SLOT
+from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QApplication, QLabel
 
 app = QApplication()
 label = QLabel("1")
 label.show()
-QTimer.singleShot(1000, app, SLOT("quit()"))
+QTimer.singleShot(1000, app.quit)
 app.exec()
