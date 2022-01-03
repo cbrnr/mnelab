33805573131c1dfe6691c6761c68461c149ae8be
Clemens Brunner <clemens.brunner@gmail.com>
2022-01-03 20:06:57 +0100

    Add test

---
+++ mnelab/test/test_readers.py
@@ -0,0 +1,12 @@
+import pytest
+from mnelab.io.readers import split_name_ext, supported
+
+
+@pytest.mark.parametrize("ext", supported.keys())
+def test_split_name_ext(ext):
+    fname = f"test{ext}"
+    assert split_name_ext(fname) == ("test", ext)
+
+
+def test_split_name_ext_unsupported():
+    assert split_name_ext("test.xxx") is None
