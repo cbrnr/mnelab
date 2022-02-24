428735d7f203cd35a0be7e16236ada15324b191d
Florian Hofer <hofaflo@gmail.com>
2022-02-24 20:00:36 +0100

    Add reading multiple XDF streams (#313)

--- mnelab/dialogs/xdf_streams.py
+++ mnelab/dialogs/xdf_streams.py
@@ -2,18 +2,23 @@
 #
 # License: BSD (3-clause)
 
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QStandardItem, QStandardItemModel
+from collections import defaultdict
+
+from PySide6.QtCore import Qt, Slot
 from PySide6.QtWidgets import (
-    QAbstractItemView,
     QCheckBox,
     QDialog,
     QDialogButtonBox,
+    QDoubleSpinBox,
     QHBoxLayout,
+    QLabel,
     QPushButton,
-    QTableView,
+    QTableWidget,
+    QTableWidgetItem,
     QVBoxLayout,
 )
+
+from .utils import IntTableWidgetItem
 
 
 class XDFStreamsDialog(QDialog):
@@ -22,63 +27,79 @@ class XDFStreamsDialog(QDialog):
         self.setWindowTitle("Select XDF Stream")
         self.fname = fname
 
-        self.model = QStandardItemModel()
-        self.model.setHorizontalHeaderLabels(["ID", "Name", "Type", "Channels", "Format",
-                                              "Sampling Rate"])
+        self.view = QTableWidget(len(rows), 6)
+        for i, row in enumerate(rows):
+            self.view.setItem(i, 0, IntTableWidgetItem(row[0]))
+            self.view.setItem(i, 1, QTableWidgetItem(row[1]))
+            self.view.setItem(i, 2, QTableWidgetItem(row[2]))
+            self.view.setItem(i, 3, IntTableWidgetItem(row[3]))
+            self.view.setItem(i, 4, QTableWidgetItem(row[4]))
+            self.view.setItem(i, 5, IntTableWidgetItem(row[5]))
+            if i in disabled:
+                for col in range(6):
+                    self.view.item(i, col).setFlags(Qt.NoItemFlags)
+        self.view.setHorizontalHeaderLabels([
+            "ID",
+            "Name",
+            "Type",
+            "Channels",
+            "Format",
+            "Sampling Rate",
+        ])
 
-        for index, stream in enumerate(rows):
-            items = []
-            for item in stream:
-                tmp = QStandardItem()
-                tmp.setData(item, Qt.DisplayRole)
-                items.append(tmp)
-            for item in items:
-                item.setEditable(False)
-                if disabled is not None and index in disabled:
-                    item.setFlags(Qt.NoItemFlags)
-            self.model.appendRow(items)
-
-        self.view = QTableView()
-        self.view.setModel(self.model)
+        self.view.setEditTriggers(QTableWidget.NoEditTriggers)
+        self.view.setSelectionBehavior(QTableWidget.SelectRows)
         self.view.verticalHeader().setVisible(False)
         self.view.horizontalHeader().setStretchLastSection(True)
         self.view.setShowGrid(False)
-        self.view.setSelectionMode(QAbstractItemView.SingleSelection)
-        self.view.setSelectionBehavior(QAbstractItemView.SelectRows)
         if selected is not None:
             self.view.selectRow(selected)
         self.view.setSortingEnabled(True)
         self.view.sortByColumn(0, Qt.AscendingOrder)
 
-        vbox = QVBoxLayout(self)
-        vbox.addWidget(self.view)
-        hbox = QHBoxLayout()
-        self._effective_srate = QCheckBox("Use effective sampling rate")
-        self._effective_srate.setChecked(True)
-        hbox.addWidget(self._effective_srate)
+        self.view.itemSelectionChanged.connect(self.toggle_buttons)
+
+        self.resample = QCheckBox()
+        self.resample_label = QLabel("Resample to:")
+        self.fs_new = QDoubleSpinBox()
+        self.fs_new.setRange(1,  max(r[5] for r in rows))
+        self.fs_new.setValue(1)
+        self.fs_new.setDecimals(1)
+        self.fs_new.setSuffix(" Hz")
+
         self._prefix_markers = QCheckBox("Prefix markers with stream ID")
         self._prefix_markers.setChecked(False)
         if len(disabled) < 2:
             self._prefix_markers.setEnabled(False)
-        hbox.addWidget(self._prefix_markers)
+
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.resample)
+        hbox1.addWidget(self.resample_label)
+        hbox1.addWidget(self.fs_new)
+        hbox1.addStretch()
+        hbox1.addWidget(self._prefix_markers)
+        hbox1.addStretch()
+
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(self.view)
+        vbox.addLayout(hbox1)
+
+        hbox2 = QHBoxLayout()
         self.details_button = QPushButton("Details")
         self.details_button.clicked.connect(self.details)
-        hbox.addWidget(self.details_button)
-        vbox.addLayout(hbox)
-
+        hbox2.addWidget(self.details_button)
+        hbox2.addStretch()
         self.buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        vbox.addWidget(self.buttonbox)
+        hbox2.addWidget(self.buttonbox)
         self.buttonbox.accepted.connect(self.accept)
         self.buttonbox.rejected.connect(self.reject)
+        vbox.addLayout(hbox2)
 
+        self.toggle_buttons()
         self.resize(775, 650)
         self.view.setColumnWidth(0, 90)
         self.view.setColumnWidth(1, 200)
         self.view.setColumnWidth(2, 140)
-
-    @property
-    def effective_srate(self):
-        return self._effective_srate.isChecked()
 
     @property
     def prefix_markers(self):
@@ -86,3 +107,34 @@ class XDFStreamsDialog(QDialog):
 
     def details(self):
         self.parent().xdf_metadata(self.fname)
+
+    @Slot()
+    def toggle_buttons(self):
+        # toggle the resample selection
+        if len(self.view.selectionModel().selectedRows()) > 1:
+            self.resample.setChecked(True)
+            self.resample.setEnabled(False)
+        else:
+            self.resample.setChecked(False)
+            self.resample.setEnabled(True)
+
+        # if there is no selection disable OK
+        if not self.view.selectionModel().selectedRows():
+            self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(False)
+        else:
+            self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(True)
+            # suggest the most common sampling rate (with the most channels)
+            channel_counts = []
+            sampling_rates = []
+            row_indices = {r.row() for r in self.view.selectedIndexes()}
+            for row in row_indices:
+                channel_counts.append(self.view.item(row, 3).value())
+                sampling_rates.append(self.view.item(row, 5).value())
+
+            counts = defaultdict(int)
+            for channel_count, sampling_rate in zip(channel_counts, sampling_rates):
+                if sampling_rate != 0:
+                    counts[sampling_rate] += channel_count
+
+            suggested_fs = max(counts, key=counts.get)
+            self.fs_new.setValue(suggested_fs)
--- mnelab/io/xdf.py
+++ mnelab/io/xdf.py
@@ -4,74 +4,148 @@ from collections import defaultdict
 
 import mne
 import numpy as np
-from pyxdf import load_xdf, match_streaminfos, resolve_streams
+import scipy.signal
+from mne.io.pick import get_channel_type_constants
+from pyxdf import load_xdf
 from pyxdf.pyxdf import _read_varlen_int, open_xdf
 
 
-def read_raw_xdf(fname, stream_id, srate="effective", prefix_markers=False, *args,
-                 **kwargs):
+def _resample_streams(streams, stream_ids, fs_new):
+    """
+    Resample multiple XDF streams to a given frequency.
+
+    Parameters
+    ----------
+    streams : dict
+        A dictionary mapping stream IDs to XDF streams.
+    stream_ids : list[int]
+        The IDs of the desired streams.
+    fs_new : float
+        Resampling target frequency in Hz.
+
+    Returns
+    -------
+    all_time_series : np.ndarray
+        Array of shape (n_samples, n_channels) containing raw data. Time intervals where a
+        stream has no data contain `np.nan`.
+    first_time : float
+        Time of the very first sample in seconds.
+    """
+    start_times = []
+    end_times = []
+    n_total_chans = 0
+    for stream_id in stream_ids:
+        start_times.append(streams[stream_id]["time_stamps"][0])
+        end_times.append(streams[stream_id]["time_stamps"][-1])
+        n_total_chans += int(streams[stream_id]["info"]["channel_count"][0])
+    first_time = min(start_times)
+    last_time = max(end_times)
+
+    n_samples = int(np.ceil((last_time - first_time) * fs_new))
+    all_time_series = np.full((n_samples, n_total_chans), np.nan)
+
+    col_start = 0
+    for stream_id in stream_ids:
+        start_time = streams[stream_id]["time_stamps"][0]
+        end_time = streams[stream_id]["time_stamps"][-1]
+        len_new = int(np.ceil((end_time - start_time) * fs_new))
+
+        x_old = streams[stream_id]["time_series"]
+        x_new = scipy.signal.resample(x_old, len_new, axis=0)
+
+        row_start = int(np.floor((streams[stream_id]["time_stamps"][0] - first_time) * fs_new))  # noqa: E501
+        row_end = row_start + x_new.shape[0]
+        col_end = col_start + x_new.shape[1]
+        all_time_series[row_start:row_end, col_start:col_end] = x_new
+
+        col_start += x_new.shape[1]
+
+    return all_time_series, first_time
+
+
+def read_raw_xdf(
+    fname,
+    stream_ids,
+    prefix_markers=False,
+    fs_new=None,
+    *args,
+    **kwargs,
+):
     """Read XDF file.
 
     Parameters
     ----------
     fname : str
         Name of the XDF file.
-    stream_id : int
-        ID (number) of the stream to load.
-    srate : {"nominal", "effective"}
-        Use either nominal or effective sampling rate.
+    stream_ids : list[int]
+        IDs of the streams to load.
     prefix_markers : bool
         Whether or not to prefix markers with their corresponding stream ID.
+    fs_new : float | None
+        Resampling target frequency in Hz. If only one stream_id is given, this can be
+        `None`, in which case no resampling is performed.
 
     Returns
     -------
     raw : mne.io.Raw
         XDF file data.
     """
-    if srate not in ("nominal", "effective"):
-        raise ValueError(f"The 'srate' parameter must be either 'nominal' or 'effective' "
-                         f"(got {srate}).")
+    if len(stream_ids) > 1 and fs_new is None:
+        raise ValueError("Parameter 'fs_new' required when reading multiple streams.")
 
     streams, _ = load_xdf(fname)
-    for stream in streams:
-        if stream["info"]["stream_id"] == stream_id:
-            break  # stream found
-    else:  # stream not found
-        raise IOError(f"Stream ID {stream_id} not found.")
-    if float(stream["info"]["nominal_srate"][0]) == 0:
-        raise RuntimeError("Importing a marker stream is not supported, try importing a "
-                           "regularly sampled stream instead.")
+    streams = {stream["info"]["stream_id"]: stream for stream in streams}
 
-    n_chans = int(stream["info"]["channel_count"][0])
-    fs = float(np.array(stream["info"][f"{srate}_srate"]).item())
-    labels, types, units = [], [], []
-    try:
-        for ch in stream["info"]["desc"][0]["channels"][0]["channel"]:
-            labels.append(str(ch["label"][0]))
-            if ch["type"]:
-                types.append(ch["type"][0])
-            units.append(ch["unit"][0] if ch["unit"] else "NA")
-    except (TypeError, IndexError):  # no channel labels found
-        pass
-    if not labels:
-        labels = [str(n) for n in range(n_chans)]
-    if not units:
-        units = ["NA" for _ in range(n_chans)]
-    info = mne.create_info(ch_names=labels, sfreq=fs, ch_types="eeg")
-    # convert from microvolts to volts if necessary
-    scale = np.array([1e-6 if u in ("microvolt", "microvolts") else 1 for u in units])
-    raw = mne.io.RawArray((stream["time_series"] * scale).T, info)
+    labels_all, types_all, units_all = [], [], []
+    for stream_id in stream_ids:
+        stream = streams[stream_id]
+
+        n_chans = int(stream["info"]["channel_count"][0])
+        labels, types, units = [], [], []
+        try:
+            for ch in stream["info"]["desc"][0]["channels"][0]["channel"]:
+                labels.append(str(ch["label"][0]))
+                if ch["type"] and ch["type"][0].lower() in get_channel_type_constants(include_defaults=True):  # noqa: E501
+                    types.append(ch["type"][0].lower())
+                else:
+                    types.append("misc")
+                units.append(ch["unit"][0] if ch["unit"] else "NA")
+        except (TypeError, IndexError):  # no channel labels found
+            pass
+        if not labels:
+            labels = [f"{stream['info']['name'][0]}_{n}" for n in range(n_chans)]
+        if not units:
+            units = ["NA" for _ in range(n_chans)]
+        if not types:
+            types = ["misc" for _ in range(n_chans)]
+        labels_all.extend(labels)
+        types_all.extend(types)
+        units_all.extend(units)
+
+    if fs_new is not None:
+        all_time_series, first_time = _resample_streams(streams, stream_ids, fs_new)
+        fs = fs_new
+    else:  # only possible if a single stream was selected
+        all_time_series = streams[stream_ids[0]]["time_series"]
+        first_time = streams[stream_ids[0]]["time_stamps"][0]
+        fs = float(np.array(stream["info"]["effective_srate"]).item())
+
+    info = mne.create_info(ch_names=labels_all, sfreq=fs, ch_types=types_all)
+
+    scale = np.array([1e-6 if u in ("microvolt", "microvolts") else 1 for u in units_all])
+    all_time_series_scaled = (all_time_series * scale).T
+
+    raw = mne.io.RawArray(all_time_series_scaled, info)
     raw._filenames = [fname]
-    first_samp = stream["time_stamps"][0]
-    markers = match_streaminfos(resolve_streams(fname), [{"type": "Markers"}])
-    for stream_id in markers:
-        for stream in streams:
-            if stream["info"]["stream_id"] == stream_id:
-                break
-        onsets = stream["time_stamps"] - first_samp
+
+    for stream_id, stream in streams.items():
+        if not (stream["info"]["nominal_srate"] == ["0"] and stream["info"]["channel_format"] == ["string"]):  # noqa: E501
+            continue
+        onsets = stream["time_stamps"] - first_time
         prefix = f"{stream_id}-" if prefix_markers else ""
         descriptions = [f"{prefix}{item}" for sub in stream["time_series"] for item in sub]
         raw.annotations.append(onsets, [0] * len(onsets), descriptions)
+
     return raw
 
 
