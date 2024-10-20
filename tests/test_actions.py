785ef819d91146cea0b9ffb642d62313e2b4f442
Clemens Brunner <clemens.brunner@gmail.com>
2024-10-20 11:20:13 +0200

    Skip Linux system packages

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -5,15 +5,15 @@
 from mnelab import MainWindow, Model
 
 
-def test_initial_actions():
+def test_initial_actions(qtbot):
     """Test if initial actions are correctly enabled/disabled."""
     model = Model()
     view = MainWindow(model)
     model.view = view
-    # qtbot.addWidget(view)
+    qtbot.addWidget(view)
 
-    # for name, action in view.actions.items():
-    #     if name in view.always_enabled:
-    #         assert action.isEnabled()
-    #     else:
-    #         assert not action.isEnabled()
+    for name, action in view.actions.items():
+        if name in view.always_enabled:
+            assert action.isEnabled()
+        else:
+            assert not action.isEnabled()
