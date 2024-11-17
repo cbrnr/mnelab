1e65e5846e373baeffa566b56364b5c7caf5af75
Benedikt A. Klöckl <bkloeckl@student.tugraz.at>
2024-11-17 13:02:08 +0100

    Change the append dialog appearance, get_compatibles and append_data functions to include original indices used for identifying the data

--- src/mnelab/dialogs/append.py
+++ src/mnelab/dialogs/append.py
@@ -9,9 +9,11 @@ from PySide6.QtWidgets import (
     QDialogButtonBox,
     QGridLayout,
     QLabel,
-    QListWidget,
     QPushButton,
     QVBoxLayout,
+    QTableWidget,
+    QTableWidgetItem,
+    QHeaderView
 )
 
 
@@ -26,23 +28,17 @@ class AppendDialog(QDialog):
         grid.addWidget(QLabel("Source"), 0, 0, Qt.AlignCenter)
         grid.addWidget(QLabel("Destination"), 0, 2, Qt.AlignCenter)
 
-        self.source = QListWidget(self)
-        self.source.setAcceptDrops(True)
-        self.source.setDragEnabled(True)
-        self.source.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        self.source.setDefaultDropAction(Qt.DropAction.MoveAction)
-        self.source.insertItems(0, [d["name"] for d in compatibles])
-        grid.addWidget(self.source, 1, 0)
+        self.source = QTableWidget(self)
+        self.setup_table(self.source, compatibles)
 
         self.move_button = QPushButton("→")
         self.move_button.setEnabled(False)
         grid.addWidget(self.move_button, 1, 1, Qt.AlignHCenter)
 
-        self.destination = QListWidget(self)
-        self.destination.setAcceptDrops(True)
-        self.destination.setDragEnabled(True)
-        self.destination.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        self.destination.setDefaultDropAction(Qt.DropAction.MoveAction)
+        self.destination = QTableWidget(self)
+        self.setup_table(self.destination, [])
+        
+        grid.addWidget(self.source, 1, 0)
         grid.addWidget(self.destination, 1, 2)
         vbox.addLayout(grid)
 
@@ -61,17 +57,34 @@ class AppendDialog(QDialog):
         self.toggle_move_source()
         self.toggle_move_destination()
 
+    def setup_table(self, table_widget, compatibles):
+        table_widget.setColumnCount(2)
+        table_widget.setHorizontalHeaderLabels(["Index", "Name"])
+        table_widget.setRowCount(len(compatibles))
+
+        for i, (idx, name) in enumerate(compatibles):
+            table_widget.setItem(i, 0, QTableWidgetItem(str(idx)))
+            table_widget.setItem(i, 1, QTableWidgetItem(name))
+
+        table_widget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        table_widget.verticalHeader().setVisible(False)
+        table_widget.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        table_widget.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+        table_widget.horizontalHeader().setStretchLastSection(True)
+
     @property
-    def names(self):
-        names = []
-        for it in range(self.destination.count()):
-            names.append(self.destination.item(it).text())
-        return names
+    def selected_idx(self):
+        """Return a list of all original indices in the destination."""
+        selected = []
+        for it in range(self.destination.rowCount()):
+            index_item = self.destination.item(it, 0)
+            if index_item:
+                selected.append(int(index_item.text()))
+        return selected
 
     @Slot()
     def toggle_ok_button(self):
-        """Toggle OK button."""
-        if self.destination.count() > 0:
+        if self.destination.rowCount() > 0:
             self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(True)
         else:
             self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(False)
@@ -96,11 +109,15 @@ class AppendDialog(QDialog):
 
     @Slot()
     def move(self):
-        if self.source.selectedItems():
-            for item in self.source.selectedItems():
-                self.destination.addItem(item.text())
-                self.source.takeItem(self.source.row(item))
-        elif self.destination.selectedItems():
-            for item in self.destination.selectedItems():
-                self.source.addItem(item.text())
-                self.destination.takeItem(self.destination.row(item))
+        source_table = self.source if self.source.selectedRanges() else self.destination
+        destination_table = self.destination if self.source.selectedRanges() else self.source
+
+        rows = sorted(set(index.row() for index in source_table.selectedIndexes()), reverse=True)
+        for row in rows:
+            idx_item = source_table.item(row, 0).clone()
+            name_item = source_table.item(row, 1).clone()
+            row_count = destination_table.rowCount()
+            destination_table.insertRow(row_count)
+            destination_table.setItem(row_count, 0, idx_item)
+            destination_table.setItem(row_count, 1, name_item)
+            source_table.removeRow(row)
