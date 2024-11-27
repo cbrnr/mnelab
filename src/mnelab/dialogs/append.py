46dd18f6400a8adcd9620c41653f43797c59bbc3
Benedikt <benedikt@Benedikts-iMac.local>
2024-11-27 12:00:55 +0100

    append dialog revert

--- src/mnelab/dialogs/append.py
+++ src/mnelab/dialogs/append.py
@@ -8,14 +8,10 @@ from PySide6.QtWidgets import (
     QDialog,
     QDialogButtonBox,
     QGridLayout,
-    QHeaderView,
     QLabel,
     QListWidget,
     QPushButton,
     QVBoxLayout,
-    QTableWidget,
-    QTableWidgetItem,
-    QHeaderView,
 )
 
 
@@ -42,10 +38,11 @@ class AppendDialog(QDialog):
         self.move_button.setEnabled(False)
         grid.addWidget(self.move_button, 1, 1, Qt.AlignHCenter)
 
-        self.destination = QTableWidget(self)
-        self.setup_table(self.destination, [])
-
-        grid.addWidget(self.source, 1, 0)
+        self.destination = QListWidget(self)
+        self.destination.setAcceptDrops(True)
+        self.destination.setDragEnabled(True)
+        self.destination.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.destination.setDefaultDropAction(Qt.DropAction.MoveAction)
         grid.addWidget(self.destination, 1, 2)
         vbox.addLayout(grid)
 
@@ -63,23 +60,6 @@ class AppendDialog(QDialog):
         self.toggle_ok_button()
         self.toggle_move_source()
         self.toggle_move_destination()
-
-    def setup_table(self, table_widget, compatibles):
-        table_widget.setColumnCount(2)
-        table_widget.setHorizontalHeaderLabels(["Index", "Name"])
-        table_widget.setRowCount(len(compatibles))
-
-        for i, (idx, name) in enumerate(compatibles):
-            table_widget.setItem(i, 0, QTableWidgetItem(str(idx)))
-            table_widget.setItem(i, 1, QTableWidgetItem(name))
-
-        table_widget.setSelectionBehavior(QAbstractItemView.SelectRows)
-        table_widget.verticalHeader().setVisible(False)
-        table_widget.horizontalHeader().setSectionResizeMode(
-            0, QHeaderView.ResizeToContents
-        )
-        table_widget.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
-        table_widget.horizontalHeader().setStretchLastSection(True)
 
     @property
     def names(self):
@@ -116,19 +96,11 @@ class AppendDialog(QDialog):
 
     @Slot()
     def move(self):
-        source_table = self.source if self.source.selectedRanges() else self.destination
-        destination_table = (
-            self.destination if self.source.selectedRanges() else self.source
-        )
-
-        rows = sorted(
-            set(index.row() for index in source_table.selectedIndexes()), reverse=True
-        )
-        for row in rows:
-            idx_item = source_table.item(row, 0).clone()
-            name_item = source_table.item(row, 1).clone()
-            row_count = destination_table.rowCount()
-            destination_table.insertRow(row_count)
-            destination_table.setItem(row_count, 0, idx_item)
-            destination_table.setItem(row_count, 1, name_item)
-            source_table.removeRow(row)
+        if self.source.selectedItems():
+            for item in self.source.selectedItems():
+                self.destination.addItem(item.text())
+                self.source.takeItem(self.source.row(item))
+        elif self.destination.selectedItems():
+            for item in self.destination.selectedItems():
+                self.source.addItem(item.text())
+                self.destination.takeItem(self.destination.row(item))
