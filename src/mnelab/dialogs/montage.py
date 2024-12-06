3af55553ebba982a5e276f1fa6afed4acc119d16
Benedikt Klöckl <benedikt.kloeckl@medunigraz.at>
2024-12-06 13:55:33 +0100

    change the set montage dialog to display the figure in the same window

--- src/mnelab/dialogs/montage.py
+++ src/mnelab/dialogs/montage.py
@@ -2,31 +2,29 @@
 #
 # License: BSD (3-clause)
 
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 from mne.channels import make_standard_montage
-from PySide6.QtCore import Qt, Slot
 from PySide6.QtWidgets import (
     QCheckBox,
     QDialog,
     QDialogButtonBox,
     QHBoxLayout,
     QListWidget,
-    QPushButton,
     QVBoxLayout,
+    QWidget,
 )
 
 
 class MontageDialog(QDialog):
-    def __init__(self, parent, montages, selected=None):
+    def __init__(self, parent, montages):
         super().__init__(parent)
         self.setWindowTitle("Set montage")
-        vbox = QVBoxLayout(self)
+
+        vbox = QVBoxLayout()
         self.montages = QListWidget()
         self.montages.insertItems(0, montages)
         self.montages.setSelectionMode(QListWidget.SingleSelection)
-        if selected is not None:
-            for i in range(self.montages.count()):
-                if self.montages.item(i).data(0) == selected:
-                    self.montages.item(i).setSelected(True)
         vbox.addWidget(self.montages)
 
         self.match_case = QCheckBox("Match case-sensitive", self)
@@ -37,35 +35,36 @@ class MontageDialog(QDialog):
         vbox.addWidget(self.match_alias)
         vbox.addWidget(self.ignore_missing)
 
-        hbox = QHBoxLayout()
-        self.view_button = QPushButton("View")
-        self.view_button.clicked.connect(self.view_montage)
-        hbox.addWidget(self.view_button)
-        hbox.addStretch()
+        controls_layout = QHBoxLayout()
+        controls_layout.addStretch()
         self.buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        hbox.addWidget(self.buttonbox)
-        vbox.addLayout(hbox)
+        controls_layout.addWidget(self.buttonbox)
+        vbox.addLayout(controls_layout)
+
         self.buttonbox.accepted.connect(self.accept)
         self.buttonbox.rejected.connect(self.reject)
-        self.montages.itemSelectionChanged.connect(self.toggle_buttons)
-        self.toggle_buttons()  # initialize OK and View buttons state
+        self.montages.itemSelectionChanged.connect(self.view_montage)
+        hbox = QHBoxLayout(self)
+        hbox.addLayout(vbox)
 
-    @Slot()
-    def toggle_buttons(self):
-        """Toggle OK and View buttons."""
-        if self.montages.selectedItems():
-            self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(True)
-            self.view_button.setEnabled(True)
-        else:
-            self.buttonbox.button(QDialogButtonBox.Ok).setEnabled(False)
-            self.view_button.setEnabled(False)
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.canvas_container = QWidget(self)
+        self.canvas_layout = QVBoxLayout(self.canvas_container)
+        self.canvas_layout.addWidget(self.canvas)
+        self.canvas_container.setLayout(self.canvas_layout)
+        hbox.addWidget(self.canvas_container)
+
+        if self.montages.count() > 0:
+            self.montages.setCurrentRow(0)
+            self.view_montage()
 
     def view_montage(self):
         name = self.montages.selectedItems()[0].data(0)
         montage = make_standard_montage(name)
-        fig = montage.plot(show_names=True, show=False)
-        win = fig.canvas.manager.window
-        win.setWindowModality(Qt.WindowModal)
-        win.setWindowTitle("Montage")
-        win.statusBar().hide()  # not necessary since matplotlib 3.3
-        fig.show()
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        montage.plot(show_names=True, show=False, axes=ax)
+        ax.set_aspect("equal")
+        self.figure.tight_layout(pad=1.0)
+        self.canvas.draw()
