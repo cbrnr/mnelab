9287e57561a5b139ca46cf9a494d13f46a4db522
Clemens Brunner <clemens.brunner@gmail.com>
2025-02-20 15:32:56 +0100

    Add option to reset (clear) settings

--- src/mnelab/settings.py
+++ src/mnelab/settings.py
@@ -49,6 +49,8 @@ def read_settings(key=None):
         settings in a dictionary.
     """
     if key is not None:
+        if key not in _DEFAULTS:
+            raise KeyError(f"Invalid setting key: {key}")
         return _get_value(key)
     return {key: _get_value(key) for key in _DEFAULTS}
 
@@ -59,6 +61,11 @@ def write_settings(**kwargs):
         if key not in _DEFAULTS:
             raise KeyError(f"Invalid setting key: {key}")
         QSettings().setValue(key, value)
+
+
+def clear_settings():
+    """Clear all settings."""
+    QSettings().clear()
 
 
 class SettingsDialog(QDialog):
@@ -80,14 +87,14 @@ class SettingsDialog(QDialog):
         grid.addWidget(QLabel("Recent files:"), 1, 0)
         self.max_recent = QSpinBox()
         self.max_recent.setRange(5, 25)
-        self.max_recent.setValue(_get_value("max_recent"))
+        self.max_recent.setValue(read_settings("max_recent"))
         self.max_recent.setAlignment(Qt.AlignRight)
         grid.addWidget(self.max_recent, 1, 1)
 
         grid.addWidget(QLabel("Displayed channels:"), 2, 0)
         self.max_channels = QSpinBox()
         self.max_channels.setRange(1, 256)
-        self.max_channels.setValue(_get_value("max_channels"))
+        self.max_channels.setValue(read_settings("max_channels"))
         self.max_channels.setAlignment(Qt.AlignRight)
         grid.addWidget(self.max_channels, 2, 1)
 
@@ -95,28 +102,15 @@ class SettingsDialog(QDialog):
         self.reset_to_defaults.clicked.connect(self.reset_settings)
         grid.addWidget(self.reset_to_defaults, 3, 0)
 
-        self.reset_to_defaults = QPushButton("Reset window")
-        self.reset_to_defaults.clicked.connect(self.reset_window)
-        grid.addWidget(self.reset_to_defaults, 4, 0)
-
-        self.reset_recent = QPushButton("Reset recent files")
-        self.reset_recent.clicked.connect(self.reset_recent_files)
-        grid.addWidget(self.reset_recent, 5, 0)
-
-        grid.addWidget(
-            QLabel(f"Settings are stored at {QSettings().fileName()}"),
-            6,
-            0,
-            1,
-            2,
-        )
+        label = QLabel(f"<i>Settings are stored in {QSettings().fileName()}.</i>")
+        grid.addWidget(label, 4, 0, 1, 2)
 
         hbox = QHBoxLayout()
         self.buttonbox = QDialogButtonBox(
             QDialogButtonBox.Apply | QDialogButtonBox.Ok | QDialogButtonBox.Cancel
         )
         hbox.addWidget(self.buttonbox)
-        grid.addLayout(hbox, 7, 0, 1, 2)
+        grid.addLayout(hbox, 5, 0, 1, 2)
 
         self.buttonbox.button(QDialogButtonBox.Apply).clicked.connect(
             self.apply_settings
@@ -128,11 +122,13 @@ class SettingsDialog(QDialog):
 
     @Slot()
     def apply_settings(self):
-        QSettings().setValue("max_recent", int(self.max_recent.text()))
-        self.parent().recent = self.parent().recent[: _get_value("max_recent")]
-        QSettings().setValue("max_channels", int(self.max_channels.text()))
-        QSettings().setValue("recent", self.parent().recent)
-        QSettings().setValue("plot_backend", self.plot_backend.currentText())
+        write_settings(
+            max_recent=int(self.max_recent.text()),
+            max_channels=int(self.max_channels.text()),
+            recent=self.parent().recent,
+            plot_backend=self.plot_backend.currentText(),
+        )
+        self.parent().recent = self.parent().recent[: read_settings("max_recent")]
 
     @Slot()
     def on_ok_clicked(self):
@@ -146,12 +142,7 @@ class SettingsDialog(QDialog):
         self.plot_backend.setCurrentIndex(
             self.plot_backend.findText(_DEFAULTS["plot_backend"])
         )
-
-    @Slot()
-    def reset_window(self):
         self.parent().resize(_DEFAULTS["size"])
         self.parent().move(_DEFAULTS["pos"])
-
-    @Slot()
-    def reset_recent_files(self):
         self.parent().recent = []
+        clear_settings()
