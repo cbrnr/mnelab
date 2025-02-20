b89c1494b105748a39c9df21e18b936136f23002
Clemens Brunner <clemens.brunner@gmail.com>
2025-02-20 15:58:40 +0100

    Add first tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,5 +1,24 @@
-from mnelab.settings import _DEFAULTS, read_settings
+import pytest
+from PySide6.QtCore import QSettings
+
+from mnelab.settings import _DEFAULTS, read_settings, write_settings
 
 
-def test_read_settings():
+@pytest.fixture(autouse=True)
+def temp_settings(tmp_path):
+    QSettings.setDefaultFormat(QSettings.Format.IniFormat)
+    QSettings.setPath(QSettings.IniFormat, QSettings.UserScope, str(tmp_path))
+    settings = QSettings()
+    settings.clear()
+    yield
+    settings.clear()
+
+
+def test_read_default_settings():
     assert read_settings() == _DEFAULTS
+    assert read_settings("max_recent") == _DEFAULTS["max_recent"]
+
+
+def test_write_and_read_settings():
+    write_settings(max_recent=10)
+    assert read_settings("max_recent") == 10
