b8a2ee68a63ffa868b507d564e9354814249892b
Clemens Brunner <clemens.brunner@gmail.com>
2025-03-06 09:21:23 +0100

    Rename test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -19,7 +19,7 @@ def test_read_default_settings():
     assert read_settings("max_recent") == _DEFAULTS["max_recent"]
 
 
-def test_write_and_read_settings():
+def test_write_read_clear_settings():
     write_settings(max_recent=10)
     assert read_settings("max_recent") == 10
     assert read_settings() == {**_DEFAULTS, "max_recent": 10}
