d7f0f6903f0fa849e319e41cffd3f4a782235f47
Clemens Brunner <clemens.brunner@gmail.com>
2025-03-11 14:24:31 +0100

    Add Windows script

---
+++ pyinstaller-windows.bat
@@ -0,0 +1,10 @@
+pyinstaller ^
+--collect-all mne ^
+--collect-all mnelab ^
+--name MNELAB ^
+--windowed ^
+--noupx ^
+--clean ^
+--noconfirm ^
+--icon src\mnelab\icons\mnelab-logo.ico ^
+src\mnelab\__main__.py
