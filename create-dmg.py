206e015c1b627acbd9c406d60cfda063f8df5c35
Clemens Brunner <clemens.brunner@gmail.com>
2025-03-12 16:32:50 +0100

    Add script to create DMG

---
+++ create-dmg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import dmgbuild
+
+dmgbuild.build_dmg(
+    filename="dist/MNELAB.dmg",
+    volume_name="MNELAB",
+    settings={
+        "format": "UDBZ",
+        "files": ["dist/MNELAB.app"],
+        "symlinks": {"Applications": "/Applications"},
+        "icon_locations": {
+            "MNELAB.app": (100, 100),
+            "Applications": (400, 100),
+        },
+        "icon_size": 128,
+        "window_rect": ((100, 100), (640, 320)),
+    },
+)
