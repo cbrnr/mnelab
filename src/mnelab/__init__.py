81be96725ffc252623e210b4ad9bd3a134fbea9d
Clemens Brunner <clemens.brunner@gmail.com>
2025-03-13 11:04:35 +0100

    Restructure

--- src/mnelab/__init__.py
+++ src/mnelab/__init__.py
@@ -7,7 +7,7 @@ import sys
 from pathlib import Path
 
 import matplotlib
-from PySide6.QtCore import QLoggingCategory, QSettings, Qt
+from PySide6.QtCore import QEvent, QLoggingCategory, QSettings, Qt
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
@@ -15,6 +15,20 @@ from mnelab.mainwindow import MainWindow
 from mnelab.model import Model
 
 __version__ = "1.1.0.dev0"
+
+
+class MNELAB(QApplication):
+    """MNELAB application."""
+
+    def __init__(self, argv):
+        super().__init__(argv)
+        self.mainwindow = None
+
+    def event(self, event):
+        if event.type() == QEvent.FileOpen:
+            self.mainwindow.open_data(event.file())
+            return True
+        return super().event(event)
 
 
 def main():
@@ -30,7 +44,7 @@ def main():
         info["CFBundleName"] = "MNELAB"
 
     matplotlib.use("QtAgg")
-    app = QApplication(sys.argv)
+    app = MNELAB(sys.argv)
     app.setApplicationName("mnelab")
     app.setApplicationDisplayName("MNELAB")
     app.setDesktopFileName("mnelab")
@@ -45,6 +59,7 @@ def main():
     QSettings.setDefaultFormat(QSettings.Format.IniFormat)
     model = Model()
     model.view = MainWindow(model)
+    app.mainwindow = model.view
     if len(sys.argv) > 1:  # open files from command line arguments
         for f in sys.argv[1:]:
             model.load(f)
