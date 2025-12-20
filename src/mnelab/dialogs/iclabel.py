307ad312282846060607ca863389119e49de6ed4
Fabian Schellander <fabian.schellander@student.tugraz.at>
2025-12-20 12:49:44 +0100

    Update dialog

--- src/mnelab/dialogs/iclabel.py
+++ src/mnelab/dialogs/iclabel.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import QSortFilterProxyModel, Qt
+from PySide6.QtCore import QEvent, QRect, QSortFilterProxyModel, Qt
 from PySide6.QtGui import QBrush, QColor, QStandardItem, QStandardItemModel
 from PySide6.QtWidgets import (
     QCheckBox,
@@ -10,6 +10,10 @@ from PySide6.QtWidgets import (
     QHeaderView,
     QLabel,
     QPushButton,
+    QStyle,
+    QStyledItemDelegate,
+    QStyleOptionButton,
+    QStyleOptionViewItem,
     QTableView,
     QVBoxLayout,
 )
@@ -24,9 +28,13 @@ class AutoSelectDialog(QDialog):
         layout = QVBoxLayout(self)
 
         grid = QGridLayout()
+
+        grid.setColumnStretch(0, 1)
+        grid.setColumnStretch(1, 0)
+        grid.setColumnStretch(2, 0)
         layout.addLayout(grid)
 
-        grid.addWidget(QLabel("Class"), 0, 0)
+        grid.addWidget(QLabel("Label"), 0, 0)
         grid.addWidget(QLabel("Threshold"), 0, 2)
 
         for idx, label in enumerate(labels):
@@ -48,8 +56,8 @@ class AutoSelectDialog(QDialog):
             spinbox.setEnabled(checkbox.isChecked())
             checkbox.toggled.connect(spinbox.setEnabled)
 
-            grid.addWidget(checkbox, row, 0)
-            grid.addWidget(larger_than, row, 1)
+            grid.addWidget(checkbox, row, 0, Qt.AlignLeft)
+            grid.addWidget(larger_than, row, 1, Qt.AlignRight)
             grid.addWidget(spinbox, row, 2)
 
             self.criteria[label] = (checkbox, spinbox)
@@ -58,6 +66,8 @@ class AutoSelectDialog(QDialog):
         self.buttonbox.accepted.connect(self.accept)
         self.buttonbox.rejected.connect(self.reject)
         layout.addWidget(self.buttonbox)
+
+        self.setFixedSize(220, self.sizeHint().height())
 
     def get_selection_rules(self):
         rules = {}
@@ -85,10 +95,79 @@ class NumberSortProxyModel(QSortFilterProxyModel):
             return str(left_data) < str(right_data)
 
 
+class CheckBoxDelegate(QStyledItemDelegate):
+    """Helperclass to center checkboxes in a QTableView."""
+
+    def paint(self, painter, option, index):
+        self.initStyleOption(option, index)
+
+        # removing the checkbox from the item view drawing
+        option.features &= ~QStyleOptionViewItem.HasCheckIndicator
+        option.widget.style().drawControl(
+            QStyle.CE_ItemViewItem, option, painter, option.widget
+        )
+
+        data = index.data(Qt.CheckStateRole)
+
+        if data is None:
+            check_state = Qt.CheckState.Unchecked
+        else:
+            check_state = Qt.CheckState(data)
+
+        opts = QStyleOptionButton()
+        opts.state |= QStyle.State_Enabled
+
+        if check_state == Qt.CheckState.Checked:
+            opts.state |= QStyle.State_On
+        else:
+            opts.state |= QStyle.State_Off
+
+        # size of the checkbox
+        s_size = (
+            option.widget.style()
+            .subElementRect(QStyle.SE_CheckBoxIndicator, opts, option.widget)
+            .size()
+        )
+
+        # center the checkbox in the cell
+        rect = option.rect
+        x = rect.x() + (rect.width() - s_size.width()) // 2
+        y = rect.y() + (rect.height() - s_size.height()) // 2
+        opts.rect = QRect(x, y, s_size.width(), s_size.height())
+
+        # draw the checkbox
+        option.widget.style().drawPrimitive(
+            QStyle.PE_IndicatorCheckBox, opts, painter, option.widget
+        )
+
+    def editorEvent(self, event, model, _option, index):
+        flags = model.flags(index)
+        if not (flags & Qt.ItemIsUserCheckable) or not (flags & Qt.ItemIsEnabled):
+            return False
+
+        if (
+            event.type() == QEvent.MouseButtonRelease
+            and event.button() == Qt.LeftButton
+        ):
+            self.toggle(model, index)
+            return True
+        return False
+
+    def toggle(self, model, index):
+        curr = index.data(Qt.CheckStateRole)
+        new_state = (
+            Qt.CheckState.Checked
+            if curr != Qt.CheckState.Checked
+            else Qt.CheckState.Unchecked
+        )
+        model.setData(index, new_state, Qt.CheckStateRole)
+
+
 class ICLabelDialog(QDialog):
     def __init__(self, parent, components_probs, exclude=None, labels=None):
         super().__init__(parent)
-        self.setWindowTitle("ICLabel")
+        self.setWindowTitle("Label ICs")
+        self.setMinimumSize(800, 500)
         if labels is None:
             self.labels = [
                 "Brain",
@@ -108,6 +187,7 @@ class ICLabelDialog(QDialog):
             exclude_set = {int(x) for x in exclude}
 
         headers = ["Component"] + self.labels + ["Exclude"]
+        self.check_col = len(headers) - 1
 
         self.model = QStandardItemModel(len(components_probs), len(headers))
         self.model.setHorizontalHeaderLabels(headers)
@@ -143,34 +223,38 @@ class ICLabelDialog(QDialog):
             )
             is_excluded = row_id in exclude_set
             state = Qt.CheckState.Checked if is_excluded else Qt.CheckState.Unchecked
-            exclude.setCheckState(state)
-            exclude.setData(int(is_excluded), Qt.UserRole)
-            exclude.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-            self.model.setItem(row_id, len(headers) - 1, exclude)
+            exclude.setData(state, Qt.CheckStateRole)
+            self.model.setItem(row_id, self.check_col, exclude)
 
         self.proxy_model = NumberSortProxyModel()
         self.proxy_model.setSourceModel(self.model)
 
         self.view = QTableView()
         self.view.setModel(self.proxy_model)
+        self.view.setItemDelegateForColumn(self.check_col, CheckBoxDelegate(self))
+
+        self.view.setSelectionMode(QTableView.NoSelection)
+        self.view.setEditTriggers(QTableView.NoEditTriggers)
+        self.view.setFocusPolicy(Qt.NoFocus)
         self.view.setSortingEnabled(True)
         self.view.setShowGrid(True)
-        self.view.setSelectionBehavior(QTableView.SelectRows)
 
         header = self.view.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(len(headers) - 1, QHeaderView.ResizeToContents)
-        for col in range(1, len(headers) - 1):
+        header.setSectionResizeMode(self.check_col, QHeaderView.ResizeToContents)
+        for col in range(1, self.check_col):
             header.setSectionResizeMode(col, QHeaderView.Stretch)
-        header.setSectionResizeMode(len(headers) - 1, QHeaderView.ResizeToContents)
 
         vbox = QVBoxLayout(self)
         vbox.addWidget(self.view)
 
         button_layout = QHBoxLayout()
         self.autoselect_button = QPushButton("Auto-Select...")
+        self.reset_button = QPushButton("Reset All")
+        self.reset_button.clicked.connect(self.reset_exclusions)
         self.autoselect_button.clicked.connect(self.open_auto_select)
         button_layout.addWidget(self.autoselect_button)
+        button_layout.addWidget(self.reset_button)
         button_layout.addStretch()
 
         self.buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
@@ -208,7 +292,7 @@ class ICLabelDialog(QDialog):
 
             exclude_item = self.model.item(row, exclude_col)
             new_state = Qt.Checked if should_exclude else Qt.Unchecked
-            exclude_item.setCheckState(new_state)
+            exclude_item.setData(new_state, Qt.CheckStateRole)
 
     def get_excluded_indices(self):
         excluded_comp = []
@@ -221,3 +305,9 @@ class ICLabelDialog(QDialog):
                 comp_id = id_item.data(Qt.UserRole)
                 excluded_comp.append(comp_id)
         return excluded_comp
+
+    def reset_exclusions(self):
+        exclude_col = self.model.columnCount() - 1
+        for row in range(self.model.rowCount()):
+            item = self.model.item(row, exclude_col)
+            item.setData(Qt.CheckState.Unchecked, Qt.CheckStateRole)
