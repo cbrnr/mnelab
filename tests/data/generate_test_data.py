5e8e03fe71a06fa58da00b05a03a4d6efa57f935
Fabian Schellander <fabian.schellander@student.tugraz.at>
2026-01-07 11:10:17 +0100

    Refactor test data generation

---
+++ tests/data/generate_test_data.py
@@ -0,0 +1,82 @@
+"""
+This script generates the data used for testing ICLabel integration.
+
+REQUIREMENTS:
+To run this script, you must have the following packages installed:
+- mne-icalabel
+- onnxruntime
+"""
+
+from pathlib import Path
+
+import mne
+import numpy as np
+from mne.preprocessing import ICA
+from mne_icalabel.iclabel.features import get_iclabel_features  # type: ignore
+from mne_icalabel.iclabel.label_components import (  # type: ignore
+    iclabel_label_components,
+)
+from mne_icalabel.iclabel.network.onnx import _format_input_for_onnx  # type: ignore
+from mne_icalabel.iclabel.network.utils import _format_input  # type: ignore
+
+ch_names = ["Fp1", "Fp2", "Fz", "Cz", "Pz", "Oz", "F7", "F8"]
+sfreq = 250
+n_components = 8
+
+out_dir = Path(".")
+montage = mne.channels.make_standard_montage("standard_1020")
+
+
+def make_raw(duration_sec):
+    np.random.seed(42)
+    data = np.random.randn(len(ch_names), sfreq * duration_sec)
+    info = mne.create_info(ch_names, sfreq, ch_types="eeg")
+    info.set_montage(montage)
+
+    raw = mne.io.RawArray(data, info)
+    raw.filter(1, 45)
+    return raw
+
+
+def fit_ica(raw):
+    ica = ICA(
+        n_components=n_components,
+        method="infomax",
+        random_state=42,
+    )
+    ica.fit(raw)
+    return ica
+
+
+def extract_features(raw, ica):
+    topo, psd, autocorr = _format_input_for_onnx(
+        *_format_input(*get_iclabel_features(raw, ica))
+    )
+    return topo, psd, autocorr
+
+
+datasets = {"continuous": 10, "short": 3, "epoched": 10}
+for name, duration in datasets.items():
+    inst = make_raw(duration_sec=duration)
+    ica = fit_ica(inst)
+
+    if name == "epoched":
+        inst = mne.make_fixed_length_epochs(inst, duration=2.0, preload=True)
+    prob = iclabel_label_components(inst, ica)
+    topo, psd, autocorr = extract_features(inst, ica)
+
+    # float32 to reduce file size
+    np.savez(
+        out_dir / f"iclabel_{name}.npz",
+        raw=inst.get_data().astype(np.float32),
+        sfreq=sfreq,
+        ch_names=inst.ch_names,
+        unmixing_matrix=ica.unmixing_matrix_.astype(np.float32),
+        pca_components=ica.pca_components_.astype(np.float32),
+        pca_mean=ica.pca_mean_.astype(np.float32),
+        n_components=n_components,
+        ref_prob=prob.astype(np.float32),
+        ref_topo=topo.astype(np.float32),
+        ref_psd=psd.astype(np.float32),
+        ref_autocorr=autocorr.astype(np.float32),
+    )
--- tests/test_iclabel.py
+++ tests/test_iclabel.py
@@ -1,76 +1,3 @@
-"""The test datasets were generated with the following code:
-
-import mne
-import numpy as np
-from mne.preprocessing import ICA
-from pathlib import Path
-from mne_icalabel.iclabel.label_components import iclabel_label_components
-from mne_icalabel.iclabel.features import get_iclabel_features
-from mne_icalabel.iclabel.network.onnx import _format_input_for_onnx
-from mne_icalabel.iclabel.network.utils import _format_input
-
-ch_names = ["Fp1", "Fp2", "Fz", "Cz", "Pz", "Oz", "F7", "F8"]
-sfreq = 250
-n_components = 8
-
-out_dir = Path(".")
-montage = mne.channels.make_standard_montage("standard_1020")
-
-
-def make_raw(duration_sec):
-    np.random.seed(42)
-    data = np.random.randn(len(ch_names), sfreq * duration_sec)
-    info = mne.create_info(ch_names, sfreq, ch_types="eeg")
-    info.set_montage(montage)
-
-    raw = mne.io.RawArray(data, info)
-    raw.filter(1, 45)
-    return raw
-
-
-def fit_ica(raw):
-    ica = ICA(
-        n_components=n_components,
-        method="infomax",
-        random_state=42,
-    )
-    ica.fit(raw)
-    return ica
-
-
-def extract_features(raw, ica):
-    topo, psd, autocorr = _format_input_for_onnx(
-        *_format_input(*get_iclabel_features(raw, ica))
-    )
-    return topo, psd, autocorr
-
-
-datasets = {"continuous": 10, "short": 3, "epoched": 10}
-for name, duration in datasets.items():
-    inst = make_raw(duration_sec=duration)
-    ica = fit_ica(inst)
-
-    if name == "epoched":
-        inst = mne.make_fixed_length_epochs(inst, duration=2.0, preload=True)
-    prob = iclabel_label_components(inst, ica)
-    topo, psd, autocorr = extract_features(inst, ica)
-
-    # float32 to reduce file size
-    np.savez(
-        out_dir / f"iclabel_{name}.npz",
-        raw=inst.get_data().astype(np.float32),
-        sfreq=sfreq,
-        ch_names=inst.ch_names,
-        unmixing_matrix=ica.unmixing_matrix_.astype(np.float32),
-        pca_components=ica.pca_components_.astype(np.float32),
-        pca_mean=ica.pca_mean_.astype(np.float32),
-        n_components=n_components,
-        ref_prob=prob.astype(np.float32),
-        ref_topo=topo.astype(np.float32),
-        ref_psd=psd.astype(np.float32),
-        ref_autocorr=autocorr.astype(np.float32),
-    )"""
-
 from pathlib import Path
 
 import mne
@@ -84,7 +11,7 @@ from mnelab.utils.labeling import _get_features, run_iclabel
 def iclabel_dataset(request):
     """Load one of the pre-generated iclabel npz datasets."""
     dataset_name = request.param
-    data_path = Path(__file__).parent / "test_data" / f"iclabel_{dataset_name}.npz"
+    data_path = Path(__file__).parent / "data" / f"iclabel_{dataset_name}.npz"
     data = np.load(data_path)
 
     info = mne.create_info(list(data["ch_names"]), data["sfreq"], ch_types="eeg")
