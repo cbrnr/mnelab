4d880544e7f68819db5cc77cdfd936e6d01fa52a
Clemens Brunner <clemens.brunner@gmail.com>
2026-08-29 11:19:22 +0200

    Include ONNX model from mnextend

--- standalone/create-standalone-windows.ps1
+++ standalone/create-standalone-windows.ps1
@@ -1,6 +1,7 @@
 pyinstaller `
     --collect-all mne `
     --collect-all mnelab `
+    --collect-all mnextend `
     --collect-all sklearn `
     --collect-all mne_qt_browser `
     --collect-all pybvrf `
